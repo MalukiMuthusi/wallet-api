4cc5472b2fb83bb36fef465ed9b39d7da4e17960
Maluki Muthusi <malukimuthusi@gmail.com>
2022-04-11 05:24:39 +0300

    Add logrus logger
    Configure the project to use logrus for logging

--- main.go
+++ main.go
@@ -4,18 +4,26 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
 	"github.com/MalukiMuthusi/wallet-api/handlers"
+	"github.com/MalukiMuthusi/wallet-api/logger"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	// Create a deadline to wait for.
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*5, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
 	r := gin.New()
+
+	gin.DebugPrintRouteFunc = DebugPrintRoute
 
 	v1 := r.Group("/v1")
 	{
@@ -42,34 +50,38 @@ func main() {
 	go func() {
 
 		if err := srv.ListenAndServe(); err != nil {
-			log.Print("failed to start")
+			logger.Log.WithFields(logrus.Fields{"serverListenError": err}).Info("server listen error")
 		}
 	}()
 
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+
 	c := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
 	signal.Notify(c, os.Interrupt)
 
 	// Block until we receive our signal.
 	<-c
 
-	// Create a deadline to wait for.
-	var wait time.Duration
+	logger.Log.WithField("shuttingDown", "shutdown").Info("shutdown server")
 
-	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
-	flag.Parse()
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), wait)
 	defer cancel()
 
 	// Doesn't block if no connections, but will otherwise wait
 	// until the timeout deadline.
 	srv.Shutdown(ctx)
-	// Optionally, you could run srv.Shutdown in a goroutine and block on
-	// <-ctx.Done() if your application should wait for other services
-	// to finalize based on context cancellation.
 
-	log.Printf("Shutting down")
 	os.Exit(0)
 
+}
+
+func DebugPrintRoute(httpMethod, absolutePath, handlerName string, nuHandlers int) {
+	logger.Log.WithFields(logrus.Fields{"httpMethod": httpMethod, "path": absolutePath, "handlerName": handlerName, "nuHandlers": nuHandlers}).Info("endpointRequest")
 }
